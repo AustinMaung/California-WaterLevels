501caae3f825eb122ac02a45e38013068c7078e2
Austin Maung <technicoal@gmail.com>
2022-12-09 17:19:32 -0800

    added cors to allow client to make requests

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 // import reactLogo from './assets/react.svg'
 // import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    function fetchData() {
+      const test = new URL("http://localhost:3000/test")
+      fetch(test, {method: 'GET', mode: 'cors'})
+      .then((res) => res.text())
+      .then((text) => console.log(text))
+    }
+    fetchData()
+  }, [])
 
   return (
     <div className="App">
--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors({
+    origin: '*'
+}))
+
 app.get('/', (req, res) => {
-    console.log('testing nodemon3')
+    // console.log("tessst")
     res.send('Server Works')
 })
 
-app.listen(3000, () => console.log("Listening on Port 3000"))
+app.get('/test', (req, res) => {
+    // console.log("get request made")
+    res.send("test success")
+})
+
+app.listen(3000, () => console.log("Listening on Port 3000 and should be cors enabled"))
